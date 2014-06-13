b6b75968d73b550a4d0abe775402665c19170901
Hiroyuki Yabe <yabe@cyber.t.u-tokyo.ac.jp>
2014-06-13 20:59:54 +0900

    顔合成のプログラム作り始め

--- app/controllers/image_processing_controller.rb
+++ app/controllers/image_processing_controller.rb
@@ -2,7 +2,7 @@ class ImageProcessingController < ApplicationController
 
   ## Front page
   def default
-    render "image_processing/default"
+      render "image_processing/default"
   end
 
   ##
@@ -28,6 +28,7 @@ class ImageProcessingController < ApplicationController
     ## Close and delete the temporary file
     temp.close
   end
+  
 
   ##
   ## Face recognition
@@ -41,15 +42,34 @@ class ImageProcessingController < ApplicationController
     img = OpenCV::IplImage.load(temp.path, OpenCV::CV_LOAD_IMAGE_COLOR)
     temp.close
 
+    ##
+    temp = File.open('app/assets/images/yoshio.jpg', 'r')
+    yoshio = OpenCV::IplImage.load(temp.path, OpenCV::CV_LOAD_IMAGE_COLOR)
+    temp.close
+
+    ## Apply the face  recognition to yoshio
+    yoshio_face = nil
+    
+    yoshio_detect = OpenCV::CvHaarClassifierCascade::load("./haarcascade_frontalface_alt.xml")
+    yoshio_detect.detect_objects(yoshio, :scale_factor => 1.1, :min_neighbors => 3).each { |rect|
+        #  yoshio.rectangle! rect.top_left, rect.bottom_right, :color => OpenCV::CvColor::Red, :thickness => 5
+        yoshio_face = IplImage.new rect.height, rect.width
+        yoshio.set_roi rect
+        yoshio.copy yoshio_face
+        yoshio.reset_roi
+    }
+
     ## Apply the face  recognition
     detector = OpenCV::CvHaarClassifierCascade::load("./haarcascade_frontalface_alt.xml")
     detector.detect_objects(img, :scale_factor => 1.1, :min_neighbors => 3).each { |rect|
-      img.rectangle! rect.top_left, rect.bottom_right, :color => OpenCV::CvColor::Red, :thickness => 5
+        #      img.rectangle! rect.top_left, rect.bottom_right, :color => OpenCV::CvColor::Red, :thickness => 5
+        #img.set_roi rect
+        #yoshio_face.copy img
     }
-
+    
     ## Create a temporary file and save the output image there
     temp = Tempfile::open(['facerecog', '.jpg'], :encoding => 'ascii-8bit')
-    img.save(temp.path)
+    yoshio_face.save(temp.path)
     ## Render the saved image
     send_file temp.path, :type => 'image/jpeg', :disposition => 'inline'
     ## Close and delete the temporary file
